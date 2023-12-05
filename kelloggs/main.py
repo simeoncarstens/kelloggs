7459eabf7ddd61db4a0444f25b28a3c9b47c6207
Simeon Carstens <simeon.carstens@tweag.io>
2023-12-05 13:49:25 +0100

    Add visualization

--- kelloggs/main.py
+++ kelloggs/main.py
@@ -2,11 +2,13 @@ from dataclasses import dataclass
 from math import sqrt
 from typing import TypeAlias
 
+import matplotlib.axes
+import matplotlib.pyplot as plt
 
 DIMENSION = 2
 BOX_WIDTH = 100.0
 SPRING_CONSTANT = 1.0
-WALL_SPRING_CONSTANT = 100.0
+WALL_SPRING_CONSTANT = 5.0
 GRAVITY_CONSTANT = 1.0
 
 Position: TypeAlias = list[float]
@@ -52,8 +54,29 @@ def update_wall_forces(particles: list[Particle]) -> None:
             particle.force[1] += WALL_SPRING_CONSTANT * elongation
 
 
+def draw_box(ax: matplotlib.axes.Axes) -> None:
+    ax.axline((0.0, 0.0), (0.0, 1000.0))
+    ax.plot((0.0, BOX_WIDTH), (0.0, 0.0))
+    ax.axline((BOX_WIDTH, 0.0), (BOX_WIDTH, 1000.0))
+
+def draw_particle(particle: Particle, ax: matplotlib.axes.Axes) -> None:
+    ax.add_patch(plt.Circle(particle.position, int(particle.radius), fill=False))  # type: ignore=
+    ax.arrow(*particle.position, *particle.force, width=1)
+
 def main() -> None:
-    pass
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    draw_box(ax)
+    particles = [
+        Particle([3.0, 3.0], 10)
+    ]
+    update_wall_forces(particles)
+    for particle in particles:
+        draw_particle(particle, ax)
+    ax.set_aspect("equal")
+    ax.set_ylim((-10, BOX_WIDTH))
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
